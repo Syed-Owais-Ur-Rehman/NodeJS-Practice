8fe8455923f6a03890a80814ce2b4017aec10807
Syed Owais Ur Rehman <52346980+Syed-Owais-Ur-Rehman@users.noreply.github.com>
2024-01-02 21:54:29 +0500

    module exports 2

--- hello.js
+++ hello.js
@@ -1,5 +1,12 @@
 const math = require("./math");
 
-console.log("math value is:", math(2, 4));
+/*const {add,sub} = require("./math")*/
 
+console.log("math value is:", math.addFn(2, 4));
+console.log("math value is:", math.subFn(2, 4));
+
+/*
+console.log("math value is:", math.add(2,4));
+console.log("math value is:", math.sub(2,4));
+*/
 console.log("Hello");
--- math.js
+++ math.js
@@ -2,4 +2,22 @@ function add(a, b) {
   return a + b;
 }
 
-module.exports = add;
+function sub(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  addFn: add,
+  subFn: sub,
+};
+
+/*module.exports = {
+  add,
+  sub,
+};
+
+will give an anonymous function
+exports.add = (a,b) => a+b;
+exports.sub = (a,b) => a-b;
+
+*/
