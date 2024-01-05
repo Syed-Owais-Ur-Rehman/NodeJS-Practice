5a23f655826af79135798895a1e7deff49c6322d
Syed Owais Ur Rehman <52346980+Syed-Owais-Ur-Rehman@users.noreply.github.com>
2024-01-06 00:34:51 +0500

    url handling

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const http = require("http");
 const fs = require("fs")
+const url = require("url")
 
 const myServer = http.createServer((req, res) => {
+    if (req.url === "/favicon.ico") return res.end()
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl)
     const log = `${Date.now()}: ${req.url} New Req Received\n`
     fs.appendFile("log.txt", log, (err, data) => {
-        switch(req.url) {
-            case '/': res.end("HomePage")
+        switch(myUrl.pathname) {
+            
+            case '/': 
+            res.end("HomePage")
             break
-            case '/about': res.end("AboutPage")
+
+            case '/about': 
+            const username = myUrl.query.myname
+            res.end(`About my friend ${username}`)
             break
-            default: res.end("404")
+
+            case '/search': 
+            const search = myUrl.query.search
+            res.end("search results are: "+ search)
+            break
+
+            default: 
+            res.end("404")
         }
     })
     // console.log(req.headers)
